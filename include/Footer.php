154c11dc68638a300ed40cc066fecfe4e93e7397
Manish0081 <95622368+Manish0081@users.noreply.github.com>
2023-04-06 01:27:54 +0530

    Add files via upload

---
+++ include/Footer.php
@@ -0,0 +1,86 @@
+<div class="footer">
+		<div class="container">
+			<div class="row">
+			
+				<div class="footer-col">
+					<div class="">COMPANY LOGO</div>
+					<h4>COMPANY</h4>
+					<ul>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">We're Hiring</a></li>
+						<li><a href="#">Terms & Conditions</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						
+					</ul> <br> 
+					
+						<ul>
+							<li><a href="#"><i class="fa-solid fa-rotate-left"></i> 15 Days return policy*</a><li>
+							<li><a href="#"><i class="fa-sharp fa-solid fa-hand-holding-dollar"></i> Cash on delivery*</a><li>
+						</ul>
+				</div>
+				
+				<div class="footer-col">
+					<h4>CUSTOMER SERVICE</h4>
+					<ul>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Track Order</a></li>
+						<li><a href="#">Return Order</a></li>
+						<li><a href="#">Cancel Order</a></li>
+					</ul><br>
+					
+					<div>
+						<h4>DOWNLOAD THE APP</h4>
+					
+						<a href="#"><img src="img/p1.webp" class="width"></a>
+						<a href="#"><img src="img/p2.webp" class="width"></a>
+					</div>
+					
+				</div>
+				
+				<div class="footer-col">
+					<h4>FOLLOW US</h4> <br>
+					<div class="social-links">
+						<a href="#"><i class="fab fa-facebook-f"></i></a>
+						<a href="#"><i class="fab fa-twitter"></i></a>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+						<a href="#"><i class="fab fa-linkedin-in"></i></a>
+					</div> <br>
+					<div class="social-links">
+						<a href="#"><i class="fab fa-google"></i></a>
+						<a href="#"><i class="fab fa-youtube"></i></a>
+						<a href="#"><i class="fab fa-telegram"></i></a>
+						<a href="#"><i class="fab fa-whatsapp"></i></a>
+					</div>
+					<div><br>
+						<h4>100% SECURE PAYMENT</h4>
+						<a href="#"><img src="img/py1.webp"></a>
+						
+					</div>
+					
+				</div>
+				<div class="footer-col">
+					<h4>CONNECT</h4>
+						<ul>
+							<li><a href="#"><i class="fa-solid fa-phone"></i> 91:00000000</a></li>
+							<li><a href="#"><i class="fa-brands fa-whatsapp"></i> 91:00000000</a></li>
+							<li><a href="#"><i class="fa-solid fa-envelope"></i> Email@gmail.com</a></li>
+							<li><a href="#"><i class="fa-solid fa-location-dot"></i> Location: </a></li>
+							
+						</ul><br>
+						<div>
+							<h4>HELP</h4>
+							<ul>
+								<li><a href="#">Support</a></li>
+								<li><a href="#">Github</a></li>
+								
+							</ul>
+							
+						</div>
+				</div>
+			</div><br><hr>
+			
+			<div>
+				<p id="copy"> © 2020 Copyright: Fydabazar.com</p>
+			</div>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	
+</head>
+<body>
+	
+	<div>
+		<?php
+			include("include/mensCart.php");
+		?>
+	</div>
+	<footer>
+		<?php
+			include("include/Footer.php");
+		?>
+	</footer>
+</body>
+
+</html>
+
+
+ 
